4b291ba45eaa43a6681e5c2c6362811369131535
Nikhil Thakur <nt34542@gmail.com>
2024-09-24 19:32:02 +0530

    Basic CRUD Operations

--- src/app.js
+++ src/app.js
@@ -3,15 +3,19 @@ const connectDB=require("./config/database")
 const app =express();
 const User = require("./models/user")
 
+app.use(express.json());
 
-app.post("/post",async (req,res)=>{
-    const user = new User({
-        firstName:"Nikhil",
-        lastName :"Thakur",
-        emailId :"nikhil@gmail.com",
-        password:"nikhil"
 
-    })
+app.post("/signup",async (req,res)=>{
+    // const user = new User({
+    //     firstName:"Nikhil",
+    //     lastName :"Thakur",
+    //     emailId :"nikhil@gmail.com",
+    //     password:"nikhil"
+
+    // })
+
+    const user =new User(req.body);
 
     try{
 
@@ -24,6 +28,77 @@ app.post("/post",async (req,res)=>{
         res.status(400).send("error saving the user :" + err.message);
 
     }
+})
+
+app.get("/user",async (req,res)=>{
+    const userEmail =req.body.emailId;
+
+    try{
+
+        const user =await User.find({emailId:userEmail});
+        if(user.length===0){
+            res.status(404).send("User Not Found..");
+        }
+        else res.send(user);
+
+    }
+    catch(err){
+        res.status(400).send("Something went wrong...");
+    }
+
+})
+
+app.get("/feed",async (req,res)=>{
+    const userEmail =req.body.emailId;
+
+    try{
+
+        const user =await User.find({});
+        if(user.length===0){
+            res.status(404).send("User Not Found..");
+        }
+        else res.send(user);
+
+    }
+    catch(err){
+        res.status(400).send("Something went wrong...");
+    }
+
+})
+
+
+app.delete("/user",async (req,res)=>{
+    const userId =req.body.userId;
+
+    try{
+        // const user =await User.findByIdAndDelete({_id:userId});
+        const user =await User.findByIdAndDelete(userId);
+        
+         res.send("User Deleted Succcesfully");
+
+    }
+    catch(err){
+        res.status(400).send("Something went wrong...");
+    }
+
+})
+
+
+app.patch("/user",async (req,res)=>{
+    const userId =req.body.userId;
+    const data = req.body;
+
+    try{
+        // const user =await User.findByIdAndDelete({_id:userId});
+        const user =await User.findByIdAndUpdate({_id:userId},data);
+        
+         res.send("User Updated Succcesfully");
+
+    }
+    catch(err){
+        res.status(400).send("Something went wrong...");
+    }
+
 })
 
 connectDB().then (()=>{
