dd6d76b9a874125bce169a4b4e206eefe22e9cde
Nikhil Thakur <nt34542@gmail.com>
2024-09-26 08:56:57 +0530

    data validations & sanitisation

--- src/app.js
+++ src/app.js
@@ -84,19 +84,35 @@ app.delete("/user",async (req,res)=>{
 })
 
 
-app.patch("/user",async (req,res)=>{
-    const userId =req.body.userId;
+app.patch("/user/:userId",async (req,res)=>{
+    const userId =req.params?.userId;
     const data = req.body;
 
+
     try{
+        const ALLOWED_UPDATES =["photoUrl","about","gender","age","skills"];
+
+        const isUpdateAllowed = Object.keys(data).every((k)=>
+        ALLOWED_UPDATES.includes(k));
+
+        if(!isUpdateAllowed){
+            throw new Error("Update Not Allowed" );
+        }
+
+        if(data.skills.length>10){
+            throw new Error("Cannot add more than 10 skills");
+        }
+
         // const user =await User.findByIdAndDelete({_id:userId});
-        const user =await User.findByIdAndUpdate({_id:userId},data);
+        const user =await User.findByIdAndUpdate({_id:userId},data,{
+            runValidators:true
+        });
         
          res.send("User Updated Succcesfully");
 
     }
     catch(err){
-        res.status(400).send("Something went wrong...");
+        res.status(400).send("Something went wrong..." +err.message);
     }
 
 })
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,59 @@
 const mongoose = require ("mongoose")
+const validator =require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName :{
-        type:String
+        type:String,
+        required:true
     },
     lastName :{
         type:String
     },
     emailId :{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email Id is not valid : " + value );
+            }
+        }
     },
     password :{
-        type:String
+        type:String,
+        required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Password is not strong : " + value );
+            }
+        }
     },
     age :{
-        type:Number
+        type:Number,
+        min:18,
+        max:70
     },
     gender :{
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender Data is not valid ..");
+            }
+        }
+    },
+    photoUrl:{
         type:String
-    }
-});
+    },
+    about :{
+        type:String,
+        default:"This is a default !"
+    },
+    skills:{
+        type:[String]
+    },
+},{timestamps:true});
 
 
 module.exports = mongoose.model("User",userSchema)
